ed71b1025fc0967a23b84614e77b20d050e3eb1c
apolo-17 <apolinar.morales.a@gmail.com>
2023-01-09 00:24:07 -0600

    Se agrego validaciond e caracteres

--- app/Http/Controllers/ZipCodesController.php
+++ app/Http/Controllers/ZipCodesController.php
@@ -17,6 +17,7 @@ class ZipCodesController extends Controller
     public function getZipCode($zipCode)
     {
         try {
+            $zipCode = strlen($zipCode) <= 4 ? '0' . $zipCode : $zipCode;
             return ZipCode::where('zip_code', $zipCode)
                 ->with(['federalEntity', 'settlements.settlementType', 'municipality'])
                 ->first(['id', 'zip_code', 'locality']);
