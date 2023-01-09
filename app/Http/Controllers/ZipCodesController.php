cf9e9f7ccc0248fd5c6011484ca8a3c824b4bf7a
apolo-17 <apolinar.morales.a@gmail.com>
2023-01-09 01:17:26 -0600

    Modificacion de query de consulta

--- app/Http/Controllers/ZipCodesController.php
+++ app/Http/Controllers/ZipCodesController.php
@@ -16,13 +16,9 @@ class ZipCodesController extends Controller
 {
     public function getZipCode($zipCode)
     {
-        try {
-            $zipCode = strlen($zipCode) <= 4 ? '0' . $zipCode : $zipCode;
-            return ZipCode::where('zip_code', $zipCode)
-                ->with(['federalEntity', 'settlements.settlementType', 'municipality'])
-                ->first(['id', 'zip_code', 'locality']);
-        } catch (\Throwable $th) {
-            return response('Not found zip code', 404);
-        }
+        $zipCode = strlen($zipCode) <= 4 ? '0' . $zipCode : $zipCode;
+        return ZipCode::with(['federalEntity', 'settlements.settlementType', 'municipality'])
+            ->where('zip_code', $zipCode)
+            ->firstOrFail(['id', 'zip_code', 'locality']);
     }
 }
