d905c447506b1405b54f9eac0aa4f30d3221936d
apolo-17 <apolinar.morales.a@gmail.com>
2023-01-09 12:10:02 -0600

    Modificacion de respnse en endpoint

--- app/Http/Controllers/ZipCodesController.php
+++ app/Http/Controllers/ZipCodesController.php
@@ -17,9 +17,11 @@ class ZipCodesController extends Controller
     public function getZipCode($zipCode)
     {
         $zipCode = strlen($zipCode) <= 4 ? '0' . $zipCode : $zipCode;
-        return ZipCode::with(['federalEntity', 'settlements.settlementType', 'municipality'])
+        /* $tmp = ZipCode::with(['federalEntity', 'settlements.settlementType', 'municipality'])
             ->where('zip_code', $zipCode)
-            ->firstOrFail(['id', 'zip_code', 'locality'])->toArray();
-        //return response()->json($tmp->toArray());
+            ->firstOrFail(['id', 'zip_code', 'locality']); */
+        return response()->json(ZipCode::with(['federalEntity', 'settlements.settlementType', 'municipality'])
+            ->where('zip_code', $zipCode)
+            ->firstOrFail(['id', 'zip_code', 'locality'])->toArray());
     }
 }
