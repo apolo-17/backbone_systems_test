5f669f1009ba5f027b0ac756621ef74c630e61bf
apolo-17 <apolinar.morales.a@gmail.com>
2023-01-09 13:01:49 -0600

    Se agrego convercion de mayusculas

--- app/Http/Service/StoreZipCodes.php
+++ app/Http/Service/StoreZipCodes.php
@@ -24,27 +24,27 @@ class StoreZipCodes
 
             for ($i = 1; $i < sizeof($array_csv); $i++) {
 
-                $zip_code = ZipCode::firstOrCreate(['zip_code' => $array_csv[$i][0]], ['locality' => strtoupper($array_csv[$i][5])]);
+                $zip_code = ZipCode::firstOrCreate(['zip_code' => $array_csv[$i][0]], ['locality' => $this->changeFormatName(strtoupper($array_csv[$i][5]))]);
 
-                $settlement_type = SettlementType::firstOrCreate(['name' => ucfirst($array_csv[$i][2])]);
+                $settlement_type = SettlementType::firstOrCreate(['name' => $this->changeFormatName(ucfirst($array_csv[$i][2]))]);
 
                 Settlement::create([
                     'key' => intval($array_csv[$i][12]),
-                    'name' => strtoupper($array_csv[$i][1]),
-                    'zone_type' => strtoupper($array_csv[$i][13]),
+                    'name' => $this->changeFormatName(strtoupper($array_csv[$i][1])),
+                    'zone_type' => $this->changeFormatName(strtoupper($array_csv[$i][13])),
                     'zip_code_id' => $zip_code->id,
                     'settlement_type_id' => $settlement_type->id
                 ]);
 
                 FederalEntity::create([
                     'key' => $array_csv[$i][7],
-                    'name' => strtoupper($array_csv[$i][4]),
+                    'name' => $this->changeFormatName(strtoupper($array_csv[$i][4])),
                     'zip_code_id' => $zip_code->id
                 ]);
 
                 Municipality::create([
                     'key' => $array_csv[$i][11],
-                    'name' => strtoupper($array_csv[$i][3]),
+                    'name' => $this->changeFormatName(strtoupper($array_csv[$i][3])),
                     'zip_code_id' => $zip_code->id
                 ]);
             }
@@ -55,5 +55,18 @@ class StoreZipCodes
             DB::rollBack();
             return response()->json([$th]);
         }
+    }
+
+    private function changeFormatName(string $name)
+    {
+        $name = strtoupper($name);
+
+        $name = str_replace(
+            array('á', 'Á', 'é', 'É', 'í', 'Í', 'ó', 'Ó', 'ú', 'Ú', 'ñ'),
+            array('A', 'A', 'E', 'E', 'I', 'I', 'O', 'O', 'U', 'U', 'Ñ'),
+            $name
+        );
+
+        return $name;
     }
 }
